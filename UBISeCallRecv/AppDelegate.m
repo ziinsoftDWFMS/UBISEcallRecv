0d02d13702c7e69d6cfc58ad9e60949b9bfceeb5
황창순 <javasoon2@hwang-ui-macbook-pro.local>
2016-02-22 19:57:21 +0900

    modify

--- UBISeCallRecv/AppDelegate.m
+++ UBISeCallRecv/AppDelegate.m
@@ -394,6 +394,7 @@ didReceiveRemoteNotification:(NSDictionary *)userInfo
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    exit(0);
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
